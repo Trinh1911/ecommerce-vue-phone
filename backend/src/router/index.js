e62f558514619994e74b793210183e8c16c6aa09
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-07 14:55:32 +0700

    AppLayout and Dashboard

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -3,11 +3,25 @@ import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
+import AppLayout from '../components/AppLayout.vue'
+import Products from "../views/Products.vue";
 const routes = [
     {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: Dashboard
+        path: '/app',
+        name: 'app',
+        component: AppLayout,
+        children: [
+            {
+                path: 'dashboard',
+                name: 'app.dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'products',
+                name: 'app.products',
+                component: Products
+              }
+        ]
     },
     {
         path: '/login',
@@ -18,12 +32,12 @@ const routes = [
         path: '/request-password',
         name: 'requestPassword',
         component: RequestPassword
-      },
-      {
+    },
+    {
         path: '/reset-password/:token',
         name: 'resetPassword',
         component: ResetPassword
-      }
+    }
 ]
 const router = createRouter({
     history: createWebHistory(),
